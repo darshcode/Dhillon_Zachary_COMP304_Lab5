2d3ca28eb685409072916ca5ddb382a9268b406f
Darshmeen Dhillon <darsh.dhillon@live.com>
2023-04-10 19:57:42 -0400

    database complete. addition will be made to accommodate login datatype

--- app/src/main/java/com/example/dhillon_zachary_comp304_lab5/MainActivity.kt
+++ app/src/main/java/com/example/dhillon_zachary_comp304_lab5/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.dhillon_zachary_comp304_lab5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "AttentionTag"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,12 +24,24 @@ class MainActivity : AppCompatActivity() {
         val myRef = database.getReference("message")
 
         //appending values.
-        //myRef.push().setValue("enter something")
-
+        //myRef.push().setValue("enter something3") --->to use this change hashset on line 36
         myRef.setValue("Hello, World!")
 
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
 
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = snapshot.getValue<String>()
+                Log.d(TAG, "Value is: " + value)
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+
+        })
 
 
     }
