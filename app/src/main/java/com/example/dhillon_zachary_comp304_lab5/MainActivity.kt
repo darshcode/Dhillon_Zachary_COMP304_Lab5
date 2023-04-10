ac0f41aba5b3193bd15130bebd61ce3adf49e8c7
Darshmeen Dhillon <darsh.dhillon@live.com>
2023-04-10 15:01:09 -0400

    initialization of the project

---
+++ app/src/main/java/com/example/dhillon_zachary_comp304_lab5/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dhillon_zachary_comp304_lab5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
