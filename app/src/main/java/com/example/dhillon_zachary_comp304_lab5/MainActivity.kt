7751d6d04e4bee31b5a80fdd9679124d755c044d
Darshmeen Dhillon <darsh.dhillon@live.com>
2023-04-10 19:19:40 -0400

    step 4 working, connected to firebase successfully. version problem determined and fixed.

--- app/src/main/java/com/example/dhillon_zachary_comp304_lab5/MainActivity.kt
+++ app/src/main/java/com/example/dhillon_zachary_comp304_lab5/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.dhillon_zachary_comp304_lab5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //step 4
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        //appending values.
+        //myRef.push().setValue("enter something")
+
+        myRef.setValue("Hello, World!")
+
+
+
+
+
     }
 }
